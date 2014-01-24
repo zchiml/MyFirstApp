e1339dc2e0cf1a7f473d4f9b4195b21f42048c73
zchiml <zchimlg@gmail.com>
2014-01-24 17:22:37 +0800

    action bar

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -23,6 +24,22 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items    
+		switch (item.getItemId()) {        
+		case R.id.action_search:            
+			//openSearch();            
+			return true;        
+			case R.id.action_settings:            
+				//openSettings();            
+				return true;        
+				default:            
+					return super.onOptionsItemSelected(item);    
+		}
+	}
+	
 	
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {    
